f39a08d34b880274893b044fc8eacd25abae2932
LucynGoosey <lnpedro03@gmail.com>
2024-03-21 03:01:31 -0400

    Added methods for loading and saving player data.

--- src/main/java/me/lucyn/fourthrealm/FourthRealmCore.java
+++ src/main/java/me/lucyn/fourthrealm/FourthRealmCore.java
@@ -1,13 +1,21 @@
 package me.lucyn.fourthrealm;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class FourthRealmCore extends JavaPlugin {
 
-    public static FourthRealmCore plugin;
+
+    public static Map<Player, RealmPlayer> playerData;
 
     @Override
     public void onEnable() {
+        playerData = new HashMap<>();
+
+        getServer().getPluginManager().registerEvents(new PlayerDataHandler(this), this);
+
         // Plugin startup logic
 
     }
---
+++ src/main/java/me/lucyn/fourthrealm/PlayerDataHandler.java
@@ -0,0 +1,73 @@
+package me.lucyn.fourthrealm;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.logging.Level;
+
+public class PlayerDataHandler implements Listener {
+
+    private final FourthRealmCore plugin;
+
+    public PlayerDataHandler(FourthRealmCore plugin) {
+        this.plugin = plugin;
+    }
+
+
+
+    public void savePlayerData(RealmPlayer realmPlayer) {
+        Player player = realmPlayer.getPlayer();
+        File playerDataFile = new File(plugin.getDataFolder(), player.getUniqueId() + ".yml");
+        FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerDataFile);
+
+        playerData.set("PlayerName", player.getName());
+        playerData.set("CurrentLivingWorld", realmPlayer.currentLivingWorld.getName());
+        playerData.set("BlessingID", realmPlayer.blessingID);
+        // Add more data as needed
+
+        try {
+            playerData.save(playerDataFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.WARNING, "Could not save player data", e);
+        }
+    }
+
+    public void loadPlayerData(Player player) {
+        File playerDataFile = new File(plugin.getDataFolder(), player.getUniqueId() + ".yml");
+        RealmPlayer realmPlayer = new RealmPlayer(player);
+
+        if(playerDataFile.exists()) {
+            FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerDataFile);
+
+
+            realmPlayer.currentLivingWorld = player.getWorld();
+            realmPlayer.blessingID = playerData.getInt("BlessingID");
+
+            if(!Objects.equals(playerData.getString("CurrentLivingWorld"), player.getWorld().getName())) {
+                plugin.getLogger().info("Player's current world does not match data file");
+            }
+        }
+        else {
+            realmPlayer.currentLivingWorld = player.getWorld();
+
+        }
+        FourthRealmCore.playerData.put(player, realmPlayer);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent event) {
+        loadPlayerData(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(org.bukkit.event.player.PlayerQuitEvent event) {
+        savePlayerData(FourthRealmCore.playerData.get(event.getPlayer()));
+        FourthRealmCore.playerData.remove(event.getPlayer());
+    }
+
+}
