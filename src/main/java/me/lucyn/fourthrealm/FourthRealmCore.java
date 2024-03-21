8b5f0257a7c067f6e9f82937377902d7b4c208f6
LucynGoosey <lnpedro03@gmail.com>
2024-03-21 01:59:48 -0400

    removed unfinished line for commit.

--- src/main/java/me/lucyn/fourthrealm/FourthRealmCore.java
+++ src/main/java/me/lucyn/fourthrealm/FourthRealmCore.java
@@ -5,7 +5,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class FourthRealmCore extends JavaPlugin {
 
     public static FourthRealmCore plugin;
-    public
 
     @Override
     public void onEnable() {
